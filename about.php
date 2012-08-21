b553a9cfbb133b40bf3149381848430071123625
Jakub Michalek <goues.cz@googlemail.com>
2012-08-21 18:18:11 +0200

    About
    Zjednodušení úvodní stránky

--- about.php
+++ about.php
@@ -1,25 +1,5 @@
 <div class="content level_two">
  <h1>O aplikaci Brejk-Stats</h1>
- <p>
-  Pokud chcete využívat aplikaci Brejk-Stats na vlastní počítači, je možnost stáhnout zdrojový kód na GitHubu (<a href="https://github.com/Goues/Brejk/" target="_blank">Odkaz</a>) a nainstalovat. V takovém případě máte zaručeno nesdílení dat o svém účtu s ostatními hráči, na který mnoho manažerů poukazuje. Zároveň výrazně snížíte zátěž této databáze.<br>
-  Stejně tak můžete přes GitHub přispět k vývoji aplikace! :)
- </p>
- <p>
-  Aplikace byla spuštěna dne <em>20. srpna 2012</em>.<br>
-  Administrátorem Brejk-Stats je manažer <a href="http://www.brejk.cz/index.php?p=info_tym&tym=63171" target="_blank">Goues (XYZ White Tiger)</a>.<br>
-  Veškeré dotazy, připomínky, doporučení, žádosti či oznámení chyb posílejte poštou na Brejku či na email goues.cz@googlemail.com.
- </p>
- <p>
-  <strong>Malá statistika</strong>:
- </p>
- <ul>
-  <li>od posledního přepočtu bylo aktualizováno
-  <?php echo dibi::query('select count(`id_team`) from `team_update` where `update` = %d', lastConversion())->fetchSingle(); ?> týmů z celkových
-  <?php echo dibi::query('select count(`id_team`) from `team`')->fetchSingle(); ?></li>
-  <li>od posledního přepočtu bylo aktualizováno
-  <?php echo dibi::query('select count(`id_player`) from `player_update` where `update` = %d', lastConversion())->fetchSingle(); ?> hráčů z celkových
-  <?php echo dibi::query('select count(`id_player`) from `player`')->fetchSingle(); ?></li>
- </ul>
  <h2>Novinky</h2>
  <p>
   21. srpna<br>
@@ -27,6 +7,11 @@
   Bylo přidáno <span class="green">stažení ligových zápasů daného týmu</span>. Ale ještě to <span class="red">není hezké</span>.<br>
   Přidáno <span class="green">zobrazení změny koeficientu a přízně fanoušků</span>.<br>
   Přidáno <span class="green">načítání TP a RP u stadionu</span>, úrovně se zobrazuzí i u detailu hráče.
+ </p>
+ <p>
+  Aplikace byla spuštěna dne <em>20. srpna 2012</em>.<br>
+  Administrátorem Brejk-Stats je manažer <a href="http://www.brejk.cz/index.php?p=info_tym&tym=63171" target="_blank">Goues (XYZ White Tiger)</a>.<br>
+  Veškeré dotazy, připomínky, doporučení, žádosti či oznámení chyb posílejte poštou na Brejku či na email goues.cz@googlemail.com.
  </p>
  <h2>Co už umí</h2>
  <p>Načíst základní informace o týmu a hráčích podle zadaného ID</p>
@@ -38,4 +23,9 @@
  <p>Načtení stadionu, a to především zázemí pro ještě snadnější práci s historií tréninku</p>
  <p>Načtení zápasů pro zobrazení výkonu hráče v jednotlivých zápasech</p>
  <p>Naštení ligových tabulek, včetně jejich historie po jednotlivých kolech</p>
+ <h2>Oznámení</h2>
+ <p>
+  Pokud chcete využívat aplikaci Brejk-Stats na vlastní počítači, je možnost stáhnout zdrojový kód na GitHubu (<a href="https://github.com/Goues/Brejk/" target="_blank">Odkaz</a>) a nainstalovat. V takovém případě máte zaručeno nesdílení dat o svém účtu s ostatními hráči, na který mnoho manažerů poukazuje. Zároveň výrazně snížíte zátěž této databáze.<br>
+  Stejně tak můžete přes GitHub přispět k vývoji aplikace! :)
+ </p>
 </div>
