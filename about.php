555d62ff413d9d81770d199cfe3d848a68ac8e75
Jakub Michalek <goues.cz@googlemail.com>
2012-08-22 14:45:21 +0200

    About

--- about.php
+++ about.php
@@ -2,6 +2,10 @@
  <h1>O aplikaci Brejk-Stats</h1>
  <h2>Novinky</h2>
  <p>
+  22. srpna<br>
+  Stadion už zobrazuje <span class="green">více informací</span>, další budou přibývat. Nové zázemí se načte až zítra, pokud už bylo od posledního přepočtu jednou načteno.
+ </p>
+ <p>
   21. srpna<br>
   Byl vylepšen způsob ukládání dat, aplikace by měla zvládnout utáhnout až <del>300 týmů</del>, oprava, asi tak 100, budu to muset ještě zlepšit. Nejlepší by bylo koupit vlastní hosting, tady mám jen 100 MB.<br>
   Bylo přidáno <span class="green">stažení ligových zápasů daného týmu</span>. Ale ještě to <span class="red">není hezké</span>.<br>
@@ -13,14 +17,9 @@
   Administrátorem Brejk-Stats je manažer <a href="http://www.brejk.cz/index.php?p=info_tym&tym=63171" target="_blank">Goues (XYZ White Tigers)</a>.<br>
   Veškeré dotazy, připomínky, doporučení, žádosti či oznámení chyb posílejte poštou na Brejku či na email goues.cz@googlemail.com.
  </p>
- <h2>Co už umí</h2>
- <p>Načíst základní informace o týmu a hráčích podle zadaného ID</p>
- <p>Zobrazobat historii hráčů a jejich tréninku <strong class="green">včetně úrovně TP a RP</strong> (ostatní zázemí brzy)</p>
- <p>Načíst ligové zápasy týmu v aktuální sezóně (částečná funkčnost)</p>
  <p><strong>Upozornění</strong>: Z důvodu nekonzistence dat během přepočtu se nová data stahují až po 23:00. Pokud tedy Brejk přepočet skončí před 23:00, uvidíte pouze neaktualizovaná data.</p>
  <h2>Co se chystá v nejbližší době</h2>
  <p>Zobrazení historie hráčů pomocí grafů</p>
- <p>Načtení stadionu, a to především zázemí pro ještě snadnější práci s historií tréninku</p>
  <p>Načtení zápasů pro zobrazení výkonu hráče v jednotlivých zápasech</p>
  <p>Naštení ligových tabulek, včetně jejich historie po jednotlivých kolech</p>
  <h2>Oznámení</h2>
